06a0c54eee4b54f2ded45ee1dc89e5e46f8cf75a
IsaacMol5 <isaacmolinasantiago@gmail.com>
2020-03-31 03:33:22 -0600

    Updates

---
+++ precache-manifest.26be26b8c5487c00de71b83a5ca011c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce50a2bf46703640b48ec1be160aebd0",
+    "url": "/IsaacMol5/pagina/index.html"
+  },
+  {
+    "revision": "861dfe9c4ad6bcd1eb4e",
+    "url": "/IsaacMol5/pagina/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff376ee7d6f20777c269",
+    "url": "/IsaacMol5/pagina/static/js/2.8819dfef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IsaacMol5/pagina/static/js/2.8819dfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861dfe9c4ad6bcd1eb4e",
+    "url": "/IsaacMol5/pagina/static/js/main.f0783f43.chunk.js"
+  },
+  {
+    "revision": "572042482b0ddfc9cb19",
+    "url": "/IsaacMol5/pagina/static/js/runtime-main.d93502d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IsaacMol5/pagina/static/media/logo.5d5d9eef.svg"
+  }
+]);
